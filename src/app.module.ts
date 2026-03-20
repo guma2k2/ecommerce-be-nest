97373aa9dee682eefed471066060f0668addd605
Thuan Ngo | Dev <thuan.ngo@689cloud.asia>
2026-03-20 23:46:18 +0700

    Update: - Migrate typeOrm to Prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
-import { ConfigModule, ConfigService } from "@nestjs/config";
-import { TypeOrmModule } from "@nestjs/typeorm";
+import { ConfigModule } from "@nestjs/config";
 import { AuthModule } from "./auth/auth.module";
 import { UserModule } from "./user/user.module";
-import { User } from "src/user/user.entity";
 import { APP_PIPE } from "@nestjs/core";
 import { ZodValidationPipe } from "nestjs-zod";
 
@@ -14,21 +12,6 @@ import { ZodValidationPipe } from "nestjs-zod";
         ConfigModule.forRoot({
             envFilePath: [`.env.${process.env.NODE_ENV}`, ".env"],
             isGlobal: true,
-        }),
-        TypeOrmModule.forRootAsync({
-            inject: [ConfigService],
-            useFactory: (configService: ConfigService) => ({
-                type: "postgres",
-                host: configService.get<string>("DATABASE_HOST"),
-                // FIXED: Ensure port is a number
-                port: configService.get<number>("DATABASE_PORT", 5432),
-                username: configService.get<string>("DATABASE_USERNAME"),
-                password: configService.get<string>("DATABASE_PASSWORD") || "password",
-                database: configService.get<string>("DATABASE_DBNAME"),
-                entities: [User],
-                // Safety: Disable synchronize in production
-                synchronize: configService.get("NODE_ENV") !== "prod",
-            }),
         }),
         AuthModule,
         UserModule,
--- src/auth/services/auth.service.ts
+++ src/auth/services/auth.service.ts
@@ -4,7 +4,6 @@ import { JwtService } from "@nestjs/jwt";
 import { SignInDto } from "src/auth/dto/sign-in.dto";
 import jwtConfig from "src/auth/config/jwt.config";
 import type { ConfigType } from "@nestjs/config";
-import { User } from "src/user/user.entity";
 import { randomUUID } from "crypto";
 import { ActiveUserData } from "src/auth/interfaces/active-user.interface";
 import { SignUpDto } from "src/auth/dto/sign-up.dto";
@@ -12,6 +11,7 @@ import { UserRole } from "src/common/enums/user.enum";
 import { BcryptService } from "src/auth/services/bcrypt.service";
 import { RefreshTokenDto } from "src/auth/dto/refresh-token-dto";
 import { RefreshTokenService } from "src/auth/services/refresh-token.service";
+import { User } from "generated/prisma/client";
 @Injectable()
 export class AuthService {
     constructor(
@@ -32,13 +32,13 @@ export class AuthService {
 
         const encryptedPassword = await this.bcryptService.hash(signUpDto.password);
         console.log("encryptedPassword", encryptedPassword);
-        const user = new User();
-        user.email = signUpDto.email;
-        user.password = encryptedPassword;
-        user.is_active = true;
-        user.first_name = signUpDto.first_name;
-        user.last_name = signUpDto.last_name;
-        user.role = UserRole.ADMIN;
+        const user: Omit<User, "id"> = {
+            email: signUpDto.email,
+            first_name: signUpDto.first_name,
+            last_name: signUpDto.last_name,
+            password: encryptedPassword,
+            is_active: true,
+        };
         await this.userService.create(user);
     }
 
@@ -63,7 +63,7 @@ export class AuthService {
         const [accessToken, refreshToken] = await Promise.all([
             this.signToken<Partial<ActiveUserData>>(user.id, this.jwtConfiguration.accessTokenTtl, {
                 email: user.email,
-                role: user.role,
+                role: UserRole.ADMIN,
             }),
             this.signToken(user.id, this.jwtConfiguration.refreshTokenTtl, {
                 refreshTokenId,
